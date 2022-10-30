c34cf81e5578902d157664a59f1dd7c19f9f2189
rhl120 <rhl120@protonmail.com>
2022-10-30 20:15:11 +0100

    Add some doc comments to the Widget trait

--- src/widgets.rs
+++ src/widgets.rs
@@ -1,5 +1,9 @@
-pub trait Widget<E> {
+///The trait that all widgets must implement
+pub trait Widget<E: crate::Event> {
+    ///Sets up the widget and returns an initial event
     fn init(&mut self) -> Option<Box<dyn FnOnce() -> E + Send + 'static>>;
+    ///Handles an event and returns another one
     fn update(&mut self, e: E) -> Option<Box<dyn FnOnce() -> E + Send + 'static>>;
+    ///Returns the string representation of a widget
     fn view(&self) -> String;
 }
