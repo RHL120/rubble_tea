c96f4c016925b5f1fd3b0e514893c1ba47cd02ed
rhl120 <rhl120@protonmail.com>
2022-10-30 23:16:01 +0100

    Add a spinner widget

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,8 @@ use termion::raw::IntoRawMode;
 pub use termion::event::Key;
 pub use termion::event::MouseButton;
 pub use termion::terminal_size;
-#[derive(Clone)]
 ///System events are the set of events that all models should support
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Clone)]
 pub enum SystemEvent {
     ///When a key has been pressed
     KeyPress(Key),
@@ -29,7 +28,7 @@ pub enum SystemEvent {
 ///This trait allows the user to create custom events.
 ///*SystemEvent* implements this trait meaning that if the programer is content
 ///with the default events, they don't have to create their own wrapper.
-pub trait Event: Eq {
+pub trait Event: Eq + Clone {
     fn from_system_event(se: SystemEvent) -> Self;
     fn to_system_event(&self) -> Option<SystemEvent>;
 }
--- src/widgets.rs
+++ src/widgets.rs
@@ -1,9 +1,61 @@
 ///The trait that all widgets must implement
-pub trait Widget<E: crate::Event> {
+pub trait Widget<E: crate::Event + Send> {
     ///Sets up the widget and returns an initial event
     fn init(&mut self) -> Option<Box<dyn FnOnce() -> E + Send + 'static>>;
     ///Handles an event and returns another one
-    fn update(&mut self, e: E) -> Option<Box<dyn FnOnce() -> E + Send + 'static>>;
+    fn update(&mut self, e: &E) -> Option<Box<dyn FnOnce() -> E + Send + 'static>>;
     ///Returns the string representation of a widget
     fn view(&self) -> String;
+}
+
+///The structure representation of a spinner
+pub struct Spinner<E: crate::Event> {
+    paused: bool,
+    update_event: E,
+    pause_event: E,
+    resume_event: E,
+    idx: usize,
+}
+
+impl<E: crate::Event + Send + 'static> Spinner<E> {
+    pub fn new(update_event: E, pause_event: E, resume_event: E) -> Self {
+        Spinner {
+            update_event,
+            pause_event,
+            resume_event,
+            paused: false,
+            idx: 0,
+        }
+    }
+}
+
+const SPINNER_FRAMES: [&str; 8] = ["⣾ ", "⣽ ", "⣻ ", "⢿ ", "⡿ ", "⣟ ", "⣯ ", "⣷ "];
+
+impl<E: crate::Event + Send + 'static> Widget<E> for Spinner<E> {
+    fn init(&mut self) -> Option<Box<dyn FnOnce() -> E + Send + 'static>> {
+        let e = self.resume_event.clone();
+        Some(Box::new(move || e))
+    }
+    fn update(&mut self, e: &E) -> Option<Box<dyn FnOnce() -> E + Send + 'static>> {
+        let e = e.clone();
+        let update = self.update_event.clone();
+        let updater = Box::new(|| {
+            std::thread::sleep(std::time::Duration::new(0, 100000000));
+            update
+        });
+        if self.update_event == e && !self.paused {
+            self.idx = (self.idx + 1) % SPINNER_FRAMES.len();
+            Some(updater)
+        } else if self.resume_event == e {
+            Some(updater)
+        } else {
+            if self.pause_event == e {
+                self.paused = true;
+            }
+            None
+        }
+    }
+    fn view(&self) -> String {
+        SPINNER_FRAMES[self.idx].to_string()
+    }
 }
